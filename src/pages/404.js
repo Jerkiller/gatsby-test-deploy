d18fef9d94a8680bb95fd6b48e69a8501089b502
luca.favorido <luca.favorido@gmil.com>
2020-09-15 15:58:15 +0200

    cao

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,17 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+let x = 'ciao';
 
 const NotFoundPage = () => (
     <Layout>
-        <SEO title="404: Not found"/>
-        <div className={"container"}>
+        <SEO title='404: Not found'/>
+        <div className={'container'}>
             <h1>NOT FOUND</h1>
             <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
         </div>
     </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
